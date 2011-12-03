3639811e8ab9fd7c1aef68e7de3eefc2fccd3ab7
Eric Kitur <kitur.eric@yahoo.com>
2011-12-03 10:19:13 +0300

    initial commit

---
+++ urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
+         {'document_root': settings.MEDIA_ROOT}),
+    )
