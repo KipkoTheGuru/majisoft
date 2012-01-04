8156fc0a9635ac2b93c6e81fbee4d811f0ae6a36
Eric Kitur <kitur.eric@yahoo.com>
2012-01-04 23:30:02 +0300

    Changed a bit of the models in the hr module

--- hr/admin.py
+++ hr/admin.py
@@ -2,10 +2,7 @@ from django.db import models
 from django.contrib import admin
 from hr.models import *
 
-class EmployeeAdmin(admin.ModelAdmin):
-    def full_name(self, employee):
-        return employee.user.get_full_name() or employee.user.username
-    
+class EmployeeAdmin(admin.ModelAdmin):    
     def groups(self, employee):
         return ", ".join([group.name for group in employee.user.groups.filter()])
     
--- hr/models.py
+++ hr/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Employee(models.Model):
     user = models.ForeignKey(User)
-    sur_name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=30)
     national_id = models.CharField(max_length=20)
     
@@ -11,4 +11,8 @@ class Employee(models.Model):
         db_table = "Employee"
     
     def __unicode__(self):
-        return self.user.get_full_name() or self.user.username
+        return self.user.get_full_name()+" %s" % self.surname or self.user.username
+    
+    @property
+    def full_name(self):
+        return self.user.get_full_name() + " "+self.surname or employee.user.username
